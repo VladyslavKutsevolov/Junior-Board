44847f46d030837a54dc218869e76609fac3b2c4
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-25 16:58:27 -0600

    initial setup

---
+++ collections/src/routes/newCollection.ts
@@ -0,0 +1,6 @@
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+
+const router = express.Router();
+
+router.post("/collections", (req: Request, res: Response) => {});
