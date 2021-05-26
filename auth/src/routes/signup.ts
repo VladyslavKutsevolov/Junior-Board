7f5ceedc91f2a51cd6e4dcaccf7f9348d7b4b834
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-25 18:14:56 -0600

    signup user route

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,50 @@
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { body } from "express-validator";
+import { User } from "../models/user";
+import { BadRequestError } from "@junior-board/common";
+
+const router = express.Router();
+
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be within 4 and 20"),
+  ],
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      throw new BadRequestError("Email in use!");
+    }
+
+    const newUser = User.build({
+      email,
+      password,
+    });
+
+    await newUser.save();
+
+    const userJWT = jwt.sign(
+      {
+        id: newUser.id,
+        email: newUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    req.session = {
+      jwt: userJWT,
+    };
+
+    res.status(201).send(newUser);
+  }
+);
+
+export { router as signUpRouter };
