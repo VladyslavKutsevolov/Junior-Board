770a6799d7c5e9cad047b1ec0e85bdae024c6278
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-26 22:09:20 -0600

    create new collection

--- collections/src/routes/newCollection.ts
+++ collections/src/routes/newCollection.ts
@@ -1,6 +1,40 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import {
+  BadRequestError,
+  requireAuth,
+  validateRequest,
+} from "@junior-board/common";
+import { Collection } from "../models/collection";
 
 const router = express.Router();
 
-router.post("/collections", (req: Request, res: Response) => {});
+router.post(
+  "/api/collections",
+  requireAuth,
+  [body("name").not().isEmpty().withMessage("Collection name is required")],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { name, description } = req.body;
+
+    const collectionName = name.toLowerCase();
+
+    const collection = await Collection.findOne({ collectionName });
+
+    if (collection && collection.userId === req.currentUser!.id) {
+      throw new BadRequestError("Collection name already exist");
+    }
+
+    const newCollection = Collection.build({
+      name,
+      description,
+      userId: req.currentUser!.id,
+    });
+
+    await newCollection.save();
+
+    res.status(201).send(newCollection);
+  }
+);
+
+export { router as newCollectionRouter };
