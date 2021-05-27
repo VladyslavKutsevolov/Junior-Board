92de39b7c01c21bdea90c220b9e443f43640e77e
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-26 22:09:02 -0600

    added error handler middleware

--- collections/src/app.ts
+++ collections/src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "express-async-errors";
 import cookieSession from "cookie-session";
+import { currentUser, errorHandler, NotFoundError } from "@junior-board/common";
+import { newCollectionRouter } from "./routes/newCollection";
 
 const app = express();
 
@@ -12,5 +14,15 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+
+app.use(currentUser);
+
+app.use(newCollectionRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 export { app };
